731d81397c413ac62e301d430e33bff5777ce7a8
unknown <vuxile@gmail.com>
2021-01-04 15:18:38 +0100

    key changed

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-require('dotenv').config();
 
 export default axios.create({
 	baseURL: 'https://www.googleapis.com/youtube/v3',
@@ -7,6 +6,6 @@ export default axios.create({
 		part: 'snippet',
 		type: 'video',
 		maxResults: 100,
-		key: process.env.REACT_APP_KEY,
+		key: 'AIzaSyATail3op31QPAgqCw96hmuM9YFZ8lJ2lU',
 	},
 });
